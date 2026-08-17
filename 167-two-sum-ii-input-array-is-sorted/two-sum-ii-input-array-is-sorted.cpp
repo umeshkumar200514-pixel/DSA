00787acd0986c1d0944c379543d5a714b02e1d2e
Umesh Kumar <umeshkumar200514@gmail.com>
2026-08-18 01:12:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (34.31%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-         // Umesh Kumar
+         // Umesh
         int n= numbers.size();
         int i=0,j=n-1;
         while (i<j){
